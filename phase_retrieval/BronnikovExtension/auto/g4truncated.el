fbc692c4ced853985f391cd0db59335fcc33c8a8
Julian Moosmann <julian.moosmann@gmail.com>
2017-06-29 15:24:00 +0200

    Initial commit.

---
+++ phase_retrieval/BronnikovExtension/auto/g4truncated.el
@@ -0,0 +1,19 @@
+(TeX-add-style-hook "g4truncated"
+ (lambda ()
+    (TeX-run-style-hooks
+     "babel"
+     "german"
+     "english"
+     "graphicx"
+     "dvips"
+     "latexsym"
+     "amssymb"
+     "amsmath"
+     "fontenc"
+     "T1"
+     "inputenc"
+     "latin1"
+     "latex2e"
+     "art10"
+     "article")))
+
